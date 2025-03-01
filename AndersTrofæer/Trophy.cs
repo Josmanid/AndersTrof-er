79b84435af0acfa44d74b940c617b8f56088e4db
Jonas Martin Smidt <jos005@edu.zealand.dk>
2025-03-01 10:30:17 +0100

    Add project files.

---
+++ AndersTrofæer/Trophy.cs
@@ -0,0 +1,33 @@
+﻿namespace AndersTrofæer
+{
+    public class Trophy
+    {
+        private string? _competition;
+        private int _year;
+        public int Id { get; set; }
+        public string? Competition
+        {
+
+            get => _competition;
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Must have a competition name");
+                }
+                if (value.Length < 3)
+                {
+                    throw new ArgumentOutOfRangeException("The competition name must be atleast 3 character long");
+                }
+               _competition = value;
+
+            }
+
+        }
+        public int Year { get; set; }
+
+
+
+
+    }
+}
