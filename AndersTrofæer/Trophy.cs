ca5776ebe3e2702c6ce7ee60816a27b99e239a91
Jonas Martin Smidt <jos005@edu.zealand.dk>
2025-03-01 13:00:14 +0100

    Opgave 1 DOne

--- AndersTrofæer/Trophy.cs
+++ AndersTrofæer/Trophy.cs
@@ -2,6 +2,7 @@
 {
     public class Trophy
     {
+       
         private string? _competition;
         private int _year;
         public int Id { get; set; }
@@ -19,15 +20,42 @@
                 {
                     throw new ArgumentOutOfRangeException("The competition name must be atleast 3 character long");
                 }
-               _competition = value;
+                _competition = value;
 
             }
 
         }
-        public int Year { get; set; }
+        public int Year
+        {
+            get => _year;
+            set
+            {
+                if (value <= 1970 || value >= 2025)
+                {
+                    throw new ArgumentOutOfRangeException($"The year should be more than 1970 and smaller than 2025");
+                }
+                _year = value;
+            }
 
+        }
+        //Paramatiserede constructor så vi kan fylde lidt på
+        public Trophy(int id, string competition, int year)
+        {
+            Id = id;
+            Competition = competition;
+            Year = year;
+        }
+        //Default constructor så vi kan få nogle værdier uden at tænke
+        public Trophy()
+        {
+            Id = 0;
+            Competition = "Unknown";
+            Year = 1985;
+        }
 
-
+        public override string ToString() {
+            return $"{Id} Anders har vundet {Competition} i år: {Year}";
+        }
 
     }
 }
---
+++ AndersTrofæerTests/TrophyTests.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndersTrofæer.Tests
+{
+    [TestClass()]
+    public class TrophyTests
+    {
+        private Trophy trophy;
+        [TestInitialize]
+
+        public void Init() {
+            //Arrange the default constructor
+            trophy = new Trophy();
+
+        }
+        [TestMethod()]
+        public void TrophyPropCompetitionTest() {
+
+            //Assert
+
+            Assert.AreEqual("Unknown", trophy.Competition);
+
+            Assert.ThrowsException<ArgumentNullException>(
+                () => trophy.Competition = null);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => trophy.Competition = "BO");
+
+
+
+        }
+
+        [DataTestMethod()]
+        [DataRow(1971)]
+        [DataRow(2024)]
+        public void TrophyPropYearShouldWorkTest(int validYear) {
+
+            //Act
+            Trophy trophyvalid = new Trophy(1, "Unknown", validYear);
+         
+            //Assert
+            Assert.AreEqual(validYear, trophyvalid.Year);
+           
+
+            ////Should not work
+            //Assert.ThrowsException<ArgumentOutOfRangeException>(
+            //    () => trophy.Year = 1960);
+            //Assert.ThrowsException<ArgumentOutOfRangeException>(
+            //    () => trophy.Year = 2030);
+            //Assert.ThrowsException<ArgumentOutOfRangeException>(
+            //() => trophy.Year = 1970);
+            //Assert.ThrowsException<ArgumentOutOfRangeException>(
+            //    () => trophy.Year = 2025);
+
+
+
+        }
+        [DataTestMethod()]
+        [DataRow(1960)]
+        [DataRow(2035)]
+        [DataRow(1970)]
+        [DataRow(2025)]
+        public void TrophyPropYearShouldNotWorkTest(int inValidYear) {
+
+            //Assert
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Trophy(1, "Unknown", inValidYear));
+
+
+        }
+
+        [TestMethod()]
+        public void ToStringTest() {
+            //Act
+            string result = trophy.ToString();
+            //Assert
+            Assert.AreEqual(result,$"0 Anders har vundet Unknown i år: 1985");
+        }
+    }
+}
